f42890350571d9604ec821b50df653987009b125
Seungduk Seo <lscitl@naver.com>
2023-01-28 04:59:49 +0900

    testing

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export MARIADB_NAME=wpdb
 export MARIADB_USER=wordpress
 export MARIADB_PASSWORD=111111
 
@@ -11,29 +12,47 @@ rm /var/lib/mysql/ib_logfile*
 rc-service mariadb start
 rc-service mariadb stop
 
-<< EOF cat >/etc/my.cnf
+cat > /etc/my.cnf << EOF
+[mysqld]
 user = root
 datadir = /var/lib/mysql
 port = 3306
-log-bin = /var/lib/mysql/mysql-bin
 bind-address = 0.0.0.0
-skip-networking = 0
 EOF
 
-/usr/bin/mysqld --user=root --bootstrap << EOF
-create database wpdb;
-create user '$MARIADB_USER'@'%' identified by '$MARIADB_PASSWORD';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON wpdb.* TO '$MARIADB_USER'@'%';
+mkdir /auth_pam_tool_dir
+touch /auth_pam_tool_dir/auth_pam_tool
 
-ALTER USER root@localhost IDENTIFIED VIA mysql_native_password USING PASSWORD("$MARIADB_PASSWORD");
-DELETE FROM mysql.user WHERE User='mariadb.sys';
-DELETE FROM mysql.user WHERE User='mysql';
+mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql --skip-test-db
+
+/usr/bin/mysqld --bootstrap << EOF
+FLUSH PRIVILEGES;
+DROP DATABASE IF EXISTS test ;
+CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;
 DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE db='test' OR db='test\\_%';
-
+DELETE FROM mysql.user WHERE User='mysql';
+CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
+GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
+exec /usr/bin/mysqld
 
-exec /usr/bin/mysqld --user=root
+# DELETE FROM mysql.user WHERE User='mariadb.sys';
+# USE mysql;
+# FLUSH PRIVILEGES;
+# GRANT ALL ON *.* TO 'root'@'%' identified by '$MARIADB_PASSWORD' WITH GRANT OPTION ;
+# GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MARIADB_PASSWORD' WITH GRANT OPTION ;
+# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MARIADB_PASSWORD}') ;
+# DELETE FROM mysql.db WHERE db='test' OR db='test\\_%';
+# FLUSH PRIVILEGES ;
+
+# CREATE DATABASE $MARIADB_NAME;
+# CREATE USER '$MARIADB_USER'@'%' identified by '$MARIADB_PASSWORD';
+# GRANT ALL ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';
+# FLUSH PRIVILEGES;
+
+# EOF
+
+
