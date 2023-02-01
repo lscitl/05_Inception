42343eea05dae7e27ad00a7a5ed008bd3d5abad0
Seungduk Seo <lscitl@naver.com>
2023-02-02 04:15:14 +0900

    test

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -10,21 +10,21 @@ mkdir /auth_pam_tool_dir
 touch /auth_pam_tool_dir/auth_pam_tool
 
 # init mariadb
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --skip-test-db
+mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql --skip-test-db
 
 # create user and set pw
-/usr/bin/mysqld -user=mysql --bootstrap << EOF
+/usr/bin/mysqld -user=root --bootstrap << EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;
 CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
 GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
 exec /usr/bin/mysqld
 
 
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 # exec /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 
 # DROP DATABASE IF EXISTS test ;
