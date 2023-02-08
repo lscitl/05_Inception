010c5a8e63f2a13c82c94079127f9d903e169302
Seungduk Seo <seseo@student.42seoul.kr>
2023-02-08 23:42:53 +0900

    apk added for bonus

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,39 +1,44 @@
 #!/bin/sh
 
-# sleep 5
+if ! $(wp core is-installed  --allow-root --path=$WP_PATH); then
+	echo "=> WordPress is not configured yet, configuring WordPress and create admin and user."
 
-if ! $(wp core is-installed  --allow-root --path="$WP_PATH"); then
-	echo "=> WordPress is not configured yet, configuring WordPress ..."
-	echo "=> Installing WordPress"
-
-	wp core download --allow-root \
-					--path="$WP_PATH"
-
+	# create config file.
 	wp config create --allow-root \
-					--path="$WP_PATH" \
-					--dbname="$MARIADB_NAME" \
-					--dbuser="$MARIADB_USER" \
-					--dbpass="$MARIADB_USER_PW" \
-					--dbhost="$MARIADB_CONTAINER_NAME" 
+					--path=$WP_PATH \
+					--dbname=$MARIADB_NAME \
+					--dbuser=$MARIADB_USER \
+					--dbpass=$MARIADB_USER_PW \
+					--dbhost=$MARIADB_CONTAINER_NAME 
 
+	# set admin
 	wp core install --allow-root \
-					--path="$WP_PATH" \
-					--url="$WP_URL" \
-					--title="$WP_TITLE" \
-					--admin_user="$WP_ADMIN" \
-					--admin_password="$WP_ADMIN_PW" \
-					--admin_email="$WP_ADMIN_EMAIL"
+					--path=$WP_PATH \
+					--url=$WP_URL \
+					--title=$WP_TITLE \
+					--admin_user=$WP_ADMIN \
+					--admin_password=$WP_ADMIN_PW \
+					--admin_email=$WP_ADMIN_EMAIL
 
+	# set user as author
 	wp user create $WP_USER $WP_USER_EMAIL \
 					--allow-root \
-					--path="$WP_PATH" \
+					--path=$WP_PATH \
 					--role=author \
 					--user_pass=$WP_USER_PW
+					<< EOF
 
-	exec php-fpm81 -F
+/** Sets up ftp. */
+define('FS_METHOD', 'ftpext');
+define('FTP_HOST', '$FTP_HOST');
+define('FTP_USER', '$FTP_USER');
+define('FTP_PASS', '$FTP_PASS');
+define('FTP_SSL', true);
+EOF
+
 else
 	echo "=> WordPress is alread configured.";
-	exec php-fpm81 -F
 fi
 
+exec php-fpm81 -F
 
