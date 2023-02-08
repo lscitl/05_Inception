6705649b3d693ca2fc5ef811bbd0c77ee07ec344
Seungduk Seo <seseo@student.42seoul.kr>
2023-02-08 23:43:19 +0900

    bug fixed

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -8,12 +8,12 @@ touch /auth_pam_tool_dir/auth_pam_tool
 mysql_install_db --user=root --basedir=/usr --datadir=/var/www/html/wpdb --skip-test-db
 
 # create user and set pw
-/usr/bin/mysqld -user=root --bootstrap << EOF
+/usr/bin/mysqld --bootstrap << EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;
 CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PW';
 GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';
-ALTER USER $MARIADB_ADMIN@'localhost' IDENTIFIED BY '$MARIADB_ADMIN_PW';
+ALTER USER '$MARIADB_ADMIN'@'localhost' IDENTIFIED BY '$MARIADB_ADMIN_PW';
 FLUSH PRIVILEGES;
 EOF
 
